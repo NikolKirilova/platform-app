6473140f02063d8c83e5440495d5d78b1efd3943
NikolKirilova <kirilova@mail.bg>
2021-11-28 13:25:01 +0200

    conditional boolean

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -73,6 +73,7 @@ const ExpenseForm = (props) => {
                   max="2023-01-01"  />
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </div>
